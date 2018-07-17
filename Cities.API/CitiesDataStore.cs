5a7f3aedf4632080e58e20c8a216a710efa45640
gatortarheel <gatortarheel@gmail.com>
2018-07-17 19:38:12 -0400

    initial

---
+++ Cities.API/CitiesDataStore.cs
@@ -0,0 +1,69 @@
+﻿using Cities.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cities.API
+{
+    public class CitiesDataStore
+    {
+        //autoproperty assignment new in C# 6
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+        
+        public CitiesDataStore()
+        {
+            //dum dum data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Richmond",
+                    Description = "The River City",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto() {
+                            Id = 1,
+                            Name = "James River",
+                            Description = "The river" },
+                        new PointOfInterestDto() {
+                            Id = 2,
+                            Name = "James River Walk",
+                            Description = "The walk next to the river"}
+                    }
+                },
+                 new CityDto()
+                {
+                    Id = 2,
+                    Name = "Glen Allen",
+                    Description = "Next to the River City",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Glen Allen River",
+                            Description = "The little river"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Glen Allen River Walk",
+                            Description = "The walk next to the little river"
+                        }
+                    }
+                },
+                  new CityDto()
+                {
+                    Id = 3,
+                    Name = "Ashland",
+                    Description = "The Center of the Universe"
+                },
+
+            };
+        }
+    }
+}
---
+++ Cities.API/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cities.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            //find city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if(cityToReturn == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(cityToReturn);
+            }
+
+        }
+    }
+}
